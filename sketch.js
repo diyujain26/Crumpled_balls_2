318752af8de194d20056e1327cf6daafdd100494
diyujain26 <67596731+diyujain26@users.noreply.github.com>
2020-08-19 09:37:07 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -52,7 +52,7 @@ function draw() {
 
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
-		Matter.Body.applyForce(ball.body,ball.body.position,{x:85,y:-85});
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:195,y:-200});
 	   
 	 }
    }
